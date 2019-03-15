Added data standardization, fix to layer normalization

dnnseg/backend.py
@@ -991,10 +991,10 @@ class HMLSTMCell(LayerRNNCell):
                                         logits=lm_logits
                                     )[..., None]
                             elif self._state_discretizer is None:
-                                lm = self._inner_activation(lm_logits)
+                                lm = lm_logits
                                 if self._return_lm_loss:
                                     lm_loss = tf.reduce_mean(
-                                        (tf.stop_gradient(tf.atanh(h_below*(1-1e-9))) - lm_logits) ** 2,
+                                        (h_below - lm) ** 2,
                                         axis=-1,
                                         keep_dims=True
                                     )
@@ -1062,33 +1062,37 @@ class HMLSTMCell(LayerRNNCell):
                             z = s[:, units * 4:]
                             s = s[:, :units * 4]
 
-                        if self._layer_normalization:
-                            s = self.norm(s, 's_ln_%d' % l)
-                        else:
+                        if not self._layer_normalization:
                             if self._implementation == 1:
                                 z += self._bias[l][:, units * 4:]
                             s = s + self._bias[l][:, :units * 4]
 
                         # Forget gate
                         f = s[:, :units]
+                        if self._layer_normalization:
+                            f = self.norm(f, 'f_ln_%d' % l)
                         f = self._recurrent_activation(f + self._forget_bias)
 
                         # Input gate
                         i = s[:, units:units * 2]
+                        if self._layer_normalization:
+                            i = self.norm(i, 'i_ln_%d' % l)
                         i = self._recurrent_activation(i)
 
                         # Output gate
                         o = s[:, units * 2:units * 3]
+                        if self._layer_normalization:
+                            o = self.norm(o, 'o_ln_%d' % l)
                         o = self._recurrent_activation(o)
 
                         # Cell proposal
                         g = s[:, units * 3:units * 4]
+                        if self._layer_normalization:
+                            g = self.norm(g, 'g_ln_%d' % l)
                         g = activation(g)
 
                         # Cell state update (forget-gated previous cell plus input-gated cell proposal)
                         c_update = f * c_behind + i * g
-                        if self._layer_normalization:
-                            c_update = self.norm(c_update, 'c_%d' % l)
 
                         # Compute cell state copy operation
                         c_copy = c_behind

dnnseg/model.py
@@ -3016,12 +3016,14 @@ class AcousticEncoderDecoder(object):
                             fd_minibatch = {
                                 self.X: X_batch,
                                 self.fixed_boundaries: fixed_boundaries_batch,
-                                self.oracle_boundaries_placeholder: oracle_boundaries_batch,
                                 self.training: False
                             }
 
                             if self.speaker_emb_dim:
                                 fd_minibatch[self.speaker] = speaker_batch
+
+                            if self.oracle_boundaries:
+                                fd_minibatch[self.oracle_boundaries_placeholder] = oracle_boundaries_batch
 
                             [segmentation_probs_cur, segmentations_cur, states_cur] = self.sess.run(
                                 [
@@ -3434,10 +3436,12 @@ class AcousticEncoderDecoder(object):
                         mask=self.segtype,
                         normalize_inputs=self.normalize_data,
                         center_inputs=self.center_data,
+                        standardize_inputs=self.standardize_data,
                         # normalize_targets=False,
                         # center_targets=False,
                         normalize_targets=self.normalize_data,
                         center_targets=self.center_data,
+                        standardize_targets=True,
                         predict_deltas=self.predict_deltas,
                         target_bwd_resampling=self.resample_targets_bwd,
                         target_fwd_resampling=self.resample_targets_fwd,
@@ -3450,6 +3454,7 @@ class AcousticEncoderDecoder(object):
                         max_len=self.max_len,
                         normalize_inputs=self.normalize_data,
                         center_inputs=self.center_data,
+                        standardize_targets=self.standardize_data,
                         predict_deltas=self.predict_deltas,
                         input_padding=self.input_padding,
                         input_resampling=self.resample_inputs,
@@ -3469,10 +3474,12 @@ class AcousticEncoderDecoder(object):
                         mask=self.segtype,
                         normalize_inputs=self.normalize_data,
                         center_inputs=self.center_data,
+                        standardize_inputs=self.standardize_data,
                         # normalize_targets=False,
                         # center_targets=False,
                         normalize_targets=self.normalize_data,
                         center_targets=self.center_data,
+                        standardize_targets=True,
                         predict_deltas=self.predict_deltas,
                         target_bwd_resampling=self.resample_targets_bwd,
                         target_fwd_resampling=self.resample_targets_fwd,
@@ -3485,6 +3492,7 @@ class AcousticEncoderDecoder(object):
                         max_len=None,
                         normalize_inputs=self.normalize_data,
                         center_inputs=self.center_data,
+                        standardize_inputs=self.standardize_data,
                         predict_deltas=self.predict_deltas,
                         input_padding=self.input_padding,
                         input_resampling=self.resample_inputs,
@@ -3498,6 +3506,7 @@ class AcousticEncoderDecoder(object):
                     mask=self.segtype,
                     normalize_inputs=self.normalize_data,
                     center_inputs=self.center_data,
+                    standardize_inputs=self.standardize_data,
                     oracle_boundaries=self.oracle_boundaries
                 )
 
@@ -3558,8 +3567,8 @@ class AcousticEncoderDecoder(object):
                         self.run_checkpoint(
                             val_data,
                             save=False,
-                            log=True,
-                            evaluate=True,
+                            log=self.log_freq > 0,
+                            evaluate=self.eval_freq > 0,
                             n_plot=n_plot,
                             ix2label=ix2label,
                             check_numerics=False,
@@ -4536,6 +4545,7 @@ class AcousticEncoderDecoderMLE(AcousticEncoderDecoder):
 
                     if self.lm_loss_scale:
                         loss += tf.add_n(self.loss_encoder_lm) * self.lm_loss_scale
+                        # loss += self.loss_encoder_lm[0] * self.lm_loss_scale
 
                 if len(self.regularizer_losses) > 0:
                     self.regularizer_loss_total = tf.add_n(self.regularizer_losses)