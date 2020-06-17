Fixed broadcasting bug when using LM window size of 1

dnnseg/backend.py
@@ -5467,7 +5467,7 @@ class AttentionalLSTMDecoderCell(LayerRNNCell):
                     if self.gaussian_attn:
                         mu = mu_prev
                         sigma2 = self.attn_sigma2
-                        ix = tf.cast(tf.range(tf.shape(self.values)[1]), dtype=tf.float32)[None, ...]
+                        ix = tf.cast(tf.range(self.values.shape[1]), dtype=tf.float32)[None, ...]
                         a = tf.exp(-(mu-ix)**2/ tf.maximum(sigma2, self.epsilon)) * self.key_val_mask
                         a /= tf.maximum(tf.reduce_sum(a, axis=1, keepdims=True), self.epsilon)
                         context_vector = tf.reduce_sum(self.values * a[..., None], axis=1)

dnnseg/model.py
@@ -1912,10 +1912,14 @@ class DNNSeg(object):
                         self.decoder_positional_encoding_type is not None and \
                         (not self.decoder_type.lower() == 'seq2seqattn' or
                          self.decoder_add_positional_encoding):
+                    if self.max_len:
+                        n_timesteps = self.max_len
+                    else:
+                        n_timesteps = tf.shape(self.X)[1] # doesn't work with prediction windows of 1, broadcasting leads to unlicensed undefined shapes
                     pe = construct_positional_encoding(
-                        tf.shape(self.X)[1],
+                        n_timesteps,
                         n_units=self.decoder_positional_encoding_units,
-                        n_batch=tf.shape(self.X)[1],
+                        n_batch=tf.shape(self.X)[0],
                         positional_encoding_type=self.decoder_positional_encoding_type,
                         positional_encoding_transform=self.decoder_positional_encoding_transform,
                         positional_encoding_activation=self.decoder_positional_encoding_activation,